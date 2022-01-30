f6fd6e1562e567d7bd0b077390a1f6a6a52c3b81
Rick <ricksmycodeguy@gmail.com>
2022-01-30 16:19:32 -0600

    wip

--- ElizaIsSilly/Program.cs
+++ ElizaIsSilly/Program.cs
@@ -6,7 +6,22 @@ namespace ElizaIsSilly
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(Doctor.Intro());
+            Console.ResetColor();
+
+            var userInput = "";
+            while (!userInput.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                userInput = Console.ReadLine();
+                string response = Doctor.response(userInput);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(response);
+                Console.ResetColor();
+
+            }
         }
     }
 }
