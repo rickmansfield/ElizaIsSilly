dccdfd29b0d79bfe1ac15d49b0cdd7cbf9dea9b4
Rick <ricksmycodeguy@gmail.com>
2022-01-30 16:05:13 -0600

    Add project files.

---
+++ ElizaIsSilly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElizaIsSilly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Doctor.Intro());
+        }
+    }
+}
